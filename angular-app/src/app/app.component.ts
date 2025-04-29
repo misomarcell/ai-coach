a2aab2dee2a0105e46eb0f85d48604a3fbb3bce7
Marcell Misó <misomarcell@gmail.com>
2025-04-29 19:53:32 +0200

    enable pwa updates

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { isPlatformBrowser } from "@angular/common";
-import { Component, inject, PLATFORM_ID } from "@angular/core";
+import { Component, inject, OnInit, PLATFORM_ID } from "@angular/core";
 import { MatIconRegistry } from "@angular/material/icon";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { DomSanitizer } from "@angular/platform-browser";
 import { RouterOutlet } from "@angular/router";
+import { SwUpdate } from "@angular/service-worker";
 import { PwaService } from "./services/pwa.service";
 
 const ICONS = [
@@ -37,13 +39,17 @@ const ICONS = [
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	private platformId = inject(PLATFORM_ID);
 	private pwaService = inject(PwaService);
+	private iconRegistry = inject(MatIconRegistry);
+	private snackBar = inject(MatSnackBar);
+	private sanitizer = inject(DomSanitizer);
+	private swUpdate = inject(SwUpdate);
 
-	constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+	constructor() {
 		for (const icon of ICONS) {
-			iconRegistry.addSvgIcon(icon, sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${icon}.svg`));
+			this.iconRegistry.addSvgIcon(icon, this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${icon}.svg`));
 		}
 
 		if (isPlatformBrowser(this.platformId)) {
@@ -52,5 +58,28 @@ export class AppComponent {
 				this.pwaService.registerPwaInstallPrompt(event);
 			});
 		}
+	}
+
+	ngOnInit(): void {
+		if (this.swUpdate.isEnabled) {
+			this.swUpdate.versionUpdates.subscribe((event) => {
+				if (event.type === "VERSION_DETECTED") {
+					console.log("New version detected:", event);
+				} else if (event.type === "VERSION_READY") {
+					console.log("New version is ready to be activated:", event);
+					this.showUpdateSnackbar();
+				} else if (event.type === "VERSION_INSTALLATION_FAILED") {
+					console.error("Version installation failed:", event);
+				}
+			});
+		}
+	}
+
+	private showUpdateSnackbar() {
+		const snackBarRef = this.snackBar.open("A new version is available!", "Reload", {
+			duration: 6000
+		});
+
+		snackBarRef.onAction().subscribe(() => window.location.reload());
 	}
 }
