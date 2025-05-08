5fa6a748c6cb9847ae1edfe19e1088987c225932
Marcell Misó <misomarcell@gmail.com>
2025-05-08 20:33:05 +0200

    analytics demo

--- angular-app/src/app/landing-page/landing-page.component.ts
+++ angular-app/src/app/landing-page/landing-page.component.ts
@@ -4,10 +4,19 @@ import { MatIconModule } from "@angular/material/icon";
 import { RouterLink } from "@angular/router";
 import { VisionDemoComponent } from "./vision-demo/vision-demo.component";
 import { ServingsDemoComponent } from "./servings-demo/servings-demo.component";
+import { AnalyticsDemoComponent } from "./analytics-demo/analytics-demo.component";
 import { FooterSectionComponent } from "./footer-section/footer-section.component";
 
 @Component({
-	imports: [RouterLink, MatButtonModule, MatIconModule, VisionDemoComponent, ServingsDemoComponent, FooterSectionComponent],
+	imports: [
+		RouterLink,
+		MatButtonModule,
+		MatIconModule,
+		VisionDemoComponent,
+		ServingsDemoComponent,
+		AnalyticsDemoComponent,
+		FooterSectionComponent
+	],
 	templateUrl: "./landing-page.component.html",
 	styleUrl: "./landing-page.component.scss"
 })
